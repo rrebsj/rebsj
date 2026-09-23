2930dab3b30361ad9833339b2e53b7793c5fd376
rrebsj <rebecca.guglielm01@gmail.com>
2026-09-23 21:59:22 +0200

    Aggiunta sezione Serie TV con menù, home e pagine secondarie placeholder.

--- menu.js
+++ menu.js
@@ -9,6 +9,7 @@
                 <li><a href="letteratura.html">Letteratura</a><ul class="submenu"><li><a href="letteratura.html">I 10 libri che mi hanno formata</a></li></ul></li>
                 <li><a href="musica.html">Musica</a><ul class="submenu"><li><a href="top-10-album.html">TOP 10 ALBUM DEL &lt;3</a></li><li><a href="questa-musica-sono-io.html">Questa musica sono io</a></li></ul></li>
                 <li><a href="podcast.html">Podcast</a><ul class="submenu"><li><a href="podcast.html">Podcast ascoltati / in ascolto</a></li></ul></li>
+                <li><a href="serie-tv.html">SERIE TV</a><ul class="submenu"><li><a href="top-10-serie-tv.html">TOP 10 SERIE TV</a></li><li><a href="archivio-cartoni-programmi-teen.html">Archivio cartoni / programmi teen</a></li><li><a href="archivio-serie-tv.html">Archivio serie tv</a></li></ul></li>
                 <li><a href="cinema.html">Cinema</a></li>
                 <li><a href="arte.html">Arte</a></li>
                 <li><a href="chi-sono.html">CONTATTI🐸</a></li>
